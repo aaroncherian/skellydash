c4f369a3ee1ccd4d32b586fb42b108c208fa1107
aaroncherian <aaron.cherian2@gmail.com>
2023-09-13 20:20:29 -0400

    adding a side bar

--- skellydash/skellycheck/layout/cards/marker_buttons_card.py
+++ skellydash/skellycheck/layout/cards/marker_buttons_card.py
@@ -1,18 +1,27 @@
 import dash_bootstrap_components as dbc
 from dash import html
 
-from.create_card import create_card
 
 def get_marker_buttons_card(marker_list, color_of_cards):
-    content = [
-        dbc.Row([
-            dbc.Col([
-                html.Div(
-                    id='marker-list',
-                    children=marker_list,
-                    style={'display': 'flex', 'flexWrap': 'wrap'}
-                )
+    return dbc.Card([
+        html.Div([
+            html.H2("Marker List", className="text-primary"),
+            dbc.Row([
+                dbc.Col([
+                    html.Div(
+                        id='marker-list',
+                        children=marker_list,
+                        style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'flex-start'}
+                    )
+                ])
             ])
-        ])
-    ]
-    return create_card("Marker List", content, color_of_cards)
+        ], style={
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'height': '100vh',
+            'overflow-y': 'auto',  # Enables vertical scrolling if the content overflows
+            'z-index': 100,  # To keep it above other elements
+            'backgroundColor': color_of_cards
+        }),
+    ], className="mb-4 mt-4")
--- skellydash/skellycheck/layout/main_layout.py
+++ skellydash/skellycheck/layout/main_layout.py
@@ -10,30 +10,43 @@ from .cards.error_shading_plot_card import get_error_shading_plot_card
 
 
 def get_layout(marker_figure, joint_rmse_figure, marker_list, gauges, color_of_cards):
-    return dbc.Container([
-        dbc.Row([
-            dbc.Col([
-                get_scatter_plot_card(marker_figure, color_of_cards),
-                get_marker_buttons_card(marker_list, color_of_cards),
-            ], md=6, style={'height': '100vh'}),
-            dbc.Col(
-                get_marker_trajectory_card(color_of_cards),
-                md=5
-            )
-        ]),
-        dbc.Row(
-            get_rmse_card(gauges)
-        ),
-        dbc.Row([
-            get_joint_rmse_plot_card(joint_rmse_figure['x_error'], 'X Dimension', color_of_cards),
-            get_joint_rmse_plot_card(joint_rmse_figure['y_error'], 'Y Dimension', color_of_cards),
-            get_joint_rmse_plot_card(joint_rmse_figure['z_error'], 'Z Dimension', color_of_cards),
-        ]),
-        dbc.Row([
-            get_absolute_error_plots_card(color_of_cards)
-        ]),
-        dbc.Row([
-            get_error_shading_plot_card(color_of_cards)
-        ]),
+    sidebar = dbc.Col(
+        [
+            get_marker_buttons_card(marker_list, color_of_cards)
+        ],
+        width={"size": 1, "order": "first"},
+        id="sidebar",
+    )
 
+    main_content = dbc.Col(
+        [
+            dbc.Row([
+                dbc.Col([
+                    get_scatter_plot_card(marker_figure, color_of_cards),
+                ], md=9),
+                dbc.Col(
+                    get_marker_trajectory_card(color_of_cards),
+                    md=3
+                ),
+            ]),
+            dbc.Row(
+                get_rmse_card(gauges)
+            ),
+            dbc.Row([
+                get_joint_rmse_plot_card(joint_rmse_figure['x_error'], 'X Dimension', color_of_cards),
+                get_joint_rmse_plot_card(joint_rmse_figure['y_error'], 'Y Dimension', color_of_cards),
+                get_joint_rmse_plot_card(joint_rmse_figure['z_error'], 'Z Dimension', color_of_cards),
+            ]),
+            dbc.Row([
+                get_absolute_error_plots_card(color_of_cards)
+            ]),
+            dbc.Row([
+                get_error_shading_plot_card(color_of_cards)
+            ]),
+        ],
+        width={"size": 9, "order": "last"},
+    )
+
+    return dbc.Container([
+        dbc.Row([sidebar, main_content]),
     ], fluid=True)
