11b07a8fc986c40e86e4dc04f9634b650f767610
aaroncherian <aaron.cherian2@gmail.com>
2023-09-20 15:42:50 -0400

    deleting unneeded things

--- skellydash/skellycheck/ui_components/dashboard.py
+++ skellydash/skellycheck/ui_components/dashboard.py
@@ -3,9 +3,6 @@ from .indicators import create_indicators_ui
 from .marker_buttons_list import create_marker_buttons
 
 from plotting.rmse_joint_plot import create_rmse_joint_bar_plot
-from plotting.absolute_error_plots import create_absolute_error_plots
-from plotting.joint_trajectory_plots import create_joint_trajectory_plots
-from plotting.shaded_error_plots import create_shaded_error_plots
 
 import numpy as np
 
@@ -20,12 +17,6 @@ def prepare_dashboard_elements(dataframe_of_3d_data, rmse_error_dataframe, frame
 
     return scatter_3d_figure, indicators, marker_buttons_list, joint_rmse_plot
 
-def update_joint_plots(selected_marker, dataframe_of_3d_data, absolute_error_dataframe, COLOR_OF_CARDS):
-    trajectory_plots = create_joint_trajectory_plots(selected_marker, dataframe_of_3d_data, COLOR_OF_CARDS)
-    asbolute_error_plots = create_absolute_error_plots(selected_marker, absolute_error_dataframe, COLOR_OF_CARDS)
-    shaded_error_plots = create_shaded_error_plots(selected_marker, dataframe_of_3d_data, absolute_error_dataframe, COLOR_OF_CARDS)
-
-    return trajectory_plots, asbolute_error_plots, shaded_error_plots
 
 def update_joint_marker_card(selected_marker, rmse_error_dataframe):
     rmses_for_this_marker = rmse_error_dataframe[rmse_error_dataframe.marker == selected_marker][['coordinate','RMSE']]
